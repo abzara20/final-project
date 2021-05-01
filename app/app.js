5ca314e07a09761024f84f1a2e6e8be3796b7899
abzara20 <azaragozapalos@gmail.com>
2021-05-01 19:37:49 -0400

    finish homepage and start setting up jquery

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import * as MODEL from "../model/model.js";
+
+function init(){
+    console.log("pog");
+};
+
+$(document).ready(function(){
+    init();
+});
---
+++ model/model.js
@@ -0,0 +1,4 @@
+var homeContent = `hi`;
+export function getPageContent(pageID) {
+    $("#app").html(eval(pageID));
+}
