41e19c46698728a19463baecbba9975b8c674905
abzara20 <azaragozapalos@gmail.com>
2021-05-01 21:39:03 -0400

    finish home, jquery nav, begin about

--- app/app.js
+++ app/app.js
@@ -1,9 +1,53 @@
 import * as MODEL from "../model/model.js";
 
 function init(){
-    console.log("pog");
+    // initial test that the function is working properly
+    // console.log("pog");
+
+    // removes black logo upon inital loading
+    $(".logo-black").css("display","none");
+
+    $("nav a").click(function(e){
+        let navID = this.id;
+        console.log(navID);
+
+        let pageID = navID + "Content";
+
+        // this will change the nav depending on whether user is on the home page or not
+        if(navID == "home") {
+
+            // this adds the hero and the text heading back in
+            $(".top").addClass("hero");
+            $(".header").css("display","block");
+
+            // removes the classes that makes the nav black
+            $("nav").removeClass("black-nav");
+            $(".links a").removeClass("black-text");
+
+            // this changes the logo from black to white, as this is the only page to have white logo
+            $(".logo-black").css("display","none");
+            $(".logo-white").css("display","block");
+
+        }else{
+
+            // removes hero and heading
+            $(".top").removeClass("hero");
+            $(".header").css("display","none");
+
+            // makes the nav black
+            $("nav").addClass("black-nav");
+            $(".links a").addClass("black-text");
+
+            // this changes the logo from white to black 
+            $(".logo-black").css("display","block");
+            $(".logo-white").css("display","none");
+        };
+
+        MODEL.getPageContent(pageID);
+    });
 };
 
 $(document).ready(function(){
     init();
+    // MODEL.getPageContent("homeContent");
 });
--- model/model.js
+++ model/model.js
@@ -1,4 +1,127 @@
-var homeContent = `hi`;
+var homeContent = `<div class="home">
+<section class="quote">
+    <div class="text-container">
+        <p>
+            “Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium totam rem aperiam eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta 
+            sunt explicabo. Iste natus error sit voluptatem accusantium doloremque 
+            laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis 
+            et quasi architecto beatae vitae dicta sunt explicabo”
+        </p>
+        <h3>John Smith</h3>
+        <h6>Corporation CEO, books author.</h6>
+    </div>
+</section>
+
+<section class="upcoming-events">
+    <div class="header">
+        <p>UPCOMING EVENTS:</p>
+    </div>
+
+    <div class="calendar">
+
+        <div class="calendar-date">
+            <div class="date-icon">
+                <h3>06</h3>
+                <p>JUN</p>
+            </div>
+
+            <div class="text-container">
+                <h4>
+                    Sed et persipiatis unde omnis iste natus
+                </h4>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aliquid temporibus quos aspernatur
+                </p>
+            </div>
+        </div>
+
+        <div class="calendar-date">
+            <div class="date-icon">
+                <h3>30</h3>
+                <p>JUL</p>
+            </div>
+
+            <div class="text-container">
+                <h4>
+                    Sed et persipiatis unde omnis iste natus
+                </h4>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aliquid temporibus quos aspernatur
+                </p>
+            </div>
+        </div>
+
+        <div class="calendar-date">
+            <div class="date-icon">
+                <h3>30</h3>
+                <p>AUG</p>
+            </div>
+
+            <div class="text-container">
+                <h4>
+                    Sed et persipiatis unde omnis iste natus
+                </h4>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aliquid temporibus quos aspernatur
+                </p>
+            </div>
+        </div>
+
+        <div class="calendar-date">
+            <div class="date-icon">
+                <h3>23</h3>
+                <p>NOV</p>
+            </div>
+
+            <div class="text-container">
+                <h4>
+                    Sed et persipiatis unde omnis iste natus
+                </h4>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aliquid temporibus quos aspernatur
+                </p>
+            </div>
+        </div>
+
+        <div class="calendar-date">
+            <div class="date-icon">
+                <h3>23</h3>
+                <p>DEC</p>
+            </div>
+
+            <div class="text-container">
+                <h4>
+                    Sed et persipiatis unde omnis iste natus
+                </h4>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aliquid temporibus quos aspernatur
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+</div>`;
+
+var aboutContent = `hi`;
+
+var galleryContent = `gallery`;
+
+var blogContent = `blog`;
+
+var contactContent = `contact`;
+
 export function getPageContent(pageID) {
     $("#app").html(eval(pageID));
 }
