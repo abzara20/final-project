d529f9880b47075f3ad8d3654fd26e4295d41718
abzara20 <azaragozapalos@gmail.com>
2021-05-01 22:49:27 -0400

    complete about and add some responsiveness to existing elements

--- model/model.js
+++ model/model.js
@@ -114,7 +114,26 @@ var homeContent = `<div class="home">
 </section>
 </div>`;
 
-var aboutContent = `hi`;
+var aboutContent = `<div class="about">
+<div class="banner">
+    <div class="image-container">
+        <div class="caption"><p>OUR HISTORY: </p></div>
+    </div>
+    <hr>
+</div>
+
+<div class="text">
+    <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto 
+        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut 
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+        sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat 
+        voluptatem. 
+    </p>
+</div>
+</div>`;
 
 var galleryContent = `gallery`;
 
